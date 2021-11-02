7c33db68e37ddaa3944282e2c8401306c366e2c2
Your Name <you@example.com>
2021-11-02 13:38:12 +0100

    Initial commit

---
+++ src/test/java/com/newenergytrading/networkcalculatorweb/NetworkCalculatorWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newenergytrading.networkcalculatorweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetworkCalculatorWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
