cc3a828d8244cff8ea79199d455090e0fcd9b701
Your Name <you@example.com>
2021-11-02 13:38:12 +0100

    Initial commit

---
+++ src/main/java/com/newenergytrading/networkcalculatorweb/NetworkCalculatorWebApplication.java
@@ -0,0 +1,13 @@
+package com.newenergytrading.networkcalculatorweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetworkCalculatorWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetworkCalculatorWebApplication.class, args);
+	}
+
+}
